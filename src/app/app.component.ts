7116d2b1d9af3a7b2eafca34dee19d85d5cf4cd0
Aveek Biswas <biswasaveek@gmail.com>
2018-08-02 15:06:02 +0530

    Starter code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'pm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  pageTitle: string = 'Acme Project Management';
 }
