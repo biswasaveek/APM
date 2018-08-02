937850fa944cbca9cd9cf7063a9a79bce6fdc279
Aveek Biswas <biswasaveek@gmail.com>
2018-08-03 00:48:25 +0530

    Adding custom pipe convertToSpaces to format 'product code'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {ProductListComponent} from './products/product-list.component';
 import {FormsModule} from '@angular/forms';
+import {ConvertToSpacesPipe} from './shared/convert-to-spaces.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
     BrowserModule,
