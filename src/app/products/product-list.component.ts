6d5e2eb8de4ebf7252f4defd691410fbbda6dbdf
Aveek Biswas <biswasaveek@gmail.com>
2018-08-02 22:04:23 +0530

    Adding styles specific to 'product-list' component

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -3,7 +3,8 @@ import {IProduct} from './product';
 
 @Component({
   selector: 'pm-products',
-  templateUrl: './product-list.component.html'
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
   pageTitle: string = 'Product List';
