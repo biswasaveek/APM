c2a081d60ae906bd2ca5f87af3017f7a23ae79f6
Aveek Biswas <biswasaveek@gmail.com>
2018-08-04 12:25:44 +0530

    Adding product.service to fetch the products list from an API

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = true;
+  errorMessage: string;
 
   hideRating: boolean = true;
   ratingMsg: string;
@@ -45,8 +46,13 @@ export class ProductListComponent implements OnInit {
 
   ngOnInit(): void {
     // console.log('OnInit ProductListComponent');
-    this.products = this.productService.getProducts();
-    this.filteredProducts = this.products;
+    this.productService.getProducts().subscribe(
+      products => {
+          this.products = products;
+          this.filteredProducts = this.products;
+      },
+      error => this.errorMessage = <any>error
+    );
   }
 
   onRatingClicked(message: string): void {
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,65 +1,35 @@
 import {Injectable} from '@angular/core';
 import {IProduct} from './product';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import {catchError, tap} from 'rxjs/operators';
 
 @Injectable()
 export class ProductService {
-  private productUrl = 'api/products/products.json';
+  private productUrl = 'assets/products/products.json';
 
   constructor(private http: HttpClient) {}
 
-  getProducts(): IProduct[] {
-    return [
-      {
-        'productId': 1,
-        'productName': 'Leaf Rake',
-        'productCode': 'GDN-0011',
-        'releaseDate': 'March 19, 2016',
-        'description': 'Leaf rake with 48-inch wooden handle.',
-        'price': 19.95,
-        'starRating': 3.2,
-        'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png'
-      },
-      {
-        'productId': 2,
-        'productName': 'Garden Cart',
-        'productCode': 'GDN-0023',
-        'releaseDate': 'March 18, 2016',
-        'description': '15 gallon capacity rolling garden cart',
-        'price': 32.99,
-        'starRating': 4.2,
-        'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png'
-      },
-      {
-        'productId': 5,
-        'productName': 'Hammer',
-        'productCode': 'TBX-0048',
-        'releaseDate': 'May 21, 2016',
-        'description': 'Curved claw steel hammer',
-        'price': 8.9,
-        'starRating': 4.8,
-        'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png'
-      },
-      {
-        'productId': 8,
-        'productName': 'Saw',
-        'productCode': 'TBX-0022',
-        'releaseDate': 'May 15, 2016',
-        'description': '15-inch steel blade hand saw',
-        'price': 11.55,
-        'starRating': 3.7,
-        'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png'
-      },
-      {
-        'productId': 10,
-        'productName': 'Video Game Controller',
-        'productCode': 'GMG-0042',
-        'releaseDate': 'October 15, 2015',
-        'description': 'Standard two-button video game controller',
-        'price': 35.95,
-        'starRating': 4.6,
-        'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png'
-      }
-    ];
+  getProducts(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>(this.productUrl).pipe(
+      tap(data => console.log('All: ' + JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    // in a real world app, we may send the server to some remote logging infrastructure
+    // instead of just logging it to the console
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
   }
 }
